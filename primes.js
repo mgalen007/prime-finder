26c03f396b0bd3bd15fafe19596cbd3904b3328d
C:\Users\HP <hyllusx@gmail.com>
2026-01-14 13:29:55 +0200

    prime-finder implementation

---
+++ primes.js
@@ -0,0 +1,13 @@
+// prime-finder algorithm implementation
+
+const primes = []
+
+for (let n = 3; n <= 100; n++) {
+    let isPrime = true
+    for (let i = 2; i <= Math.ceil(n/2); i++) {
+        if (n % i == 0) isPrime = false
+    }
+    if (isPrime) primes.push(n)
+}
+
+console.log(primes)
